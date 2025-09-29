6b4609ca427512f8ec35c28f3b7137e9155370b4
Bárbara Ayllon Lemes <85914321+BarbaraLemes@users.noreply.github.com>
2025-09-29 00:02:46 -0400

    criando componente menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Menu from "./components/Menu"
+
 function App() {
 
   return (
     <>
-      <div>
-        Casa Verde
-      </div>
+      <Menu />
     </>
   )
 }
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,7 +1,32 @@
+import styles from './Menu.module.css';
+
 export default function Menu() {
-    return (
-        <header>
-            
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <div>
+        <img src="/imagens/logo.png" alt="Logo" />
+      </div>
+      <nav>
+        <li>
+          <a href="#">Como fazer</a>
+          /
+        </li>
+        <li>
+          <a href="#">Ofertas</a>
+          /
+        </li>
+        <li>
+          <a href="#">Depoimentos</a>
+          /
+        </li>
+        <li>
+          <a href="#">Vídeos</a>
+          /
+        </li>
+        <li>
+          <a href="#">Meu carrinho</a>
+        </li>
+      </nav>
+    </header>
+  );
 }
